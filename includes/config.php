1d91c70033f6cdd8dd817f3382807a83fb371b44
Serrjik <uac.sarge@gmail.com>
2021-10-26 22:43:13 +0300

    first commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('SITE_NAME', 'Cut your URL');
+define('HOST', $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["HTTP_HOST"]);
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cut_url');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
