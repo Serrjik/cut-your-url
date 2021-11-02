2cb77f9d9936c434d0dc26a5aca5404df035690b
Serrjik <uac.sarge@gmail.com>
2021-11-02 16:54:31 +0300

    Включено отображение ошибок

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,7 @@
 <?php
+// TODO Закомментировать отображение ошибок.
+error_reporting(E_ALL && E_NOTICE);
+ini_set('display_errors', 1);
 
 define('SITE_NAME', 'Cut your URL');
 
@@ -9,6 +12,6 @@ define('HOST', $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["HTTP_HOST"]);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'cut_url');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', ''); // root
 
 session_start();
